9caafeac185d29e42e065b28741622b80c566c38
Raymond Ha <Haro@mail.uc.edu>
2022-11-06 22:46:03 -0500

    Added static formula

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+
+        int [] dataPoints = new int [100];
+
+        for(int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rnd.nextInt(100) + 1;
+        }
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i] + " | ");
+        }
+        int sum = 0;
+        double ave = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            sum += dataPoints[i];
+        }
+        System.out.println("\nThe sum of values is " + sum);
+        ave = (double) sum / dataPoints.length;
+        System.out.printf("The average of the random array dataPoints is: %5.2f", ave);
+        System.out.println("The average is: " + sum / dataPoints.length);
+
+        double linearScan= SafeInput.getRangedInt(in,"Enter an int: ", 1, 100);
+
+        int count = 0;
+        for(int i = 0; i < dataPoints.length; i++) {
+            if(dataPoints[i] == linearScan){
+                count++;
+            }
+        }
+        System.out.println(linearScan + " was found " + count + " times. ");
+
+        System.out.println("Enter the number you want to input: ");
+        int input = in.nextInt();
+
+        for(int i = 0; i < dataPoints.length; i++) {
+            count++;
+            if(dataPoints[i] == input)
+            {
+                count = count - 1;
+                System.out.println(" Value " + input + " was found at array index position " + count);
+                break;
+            }
+        }
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] < min) // New MIN!
+                min = dataPoints[i]; //so save it
+            if (dataPoints[i] > max) // New MAX!
+                max = dataPoints[i]; //so save it
+        }
+        System.out.println("Min is " + min);
+        System.out.println("Max is " + max);
+
+        System.out.println(" Average of dataPoints is: " + getAverage(dataPoints));
     }
+    public static double getAverage(int values[])
+    {
+        int sum = 0;
+        double ave = 0;
+        for(int r = 0; r < values.length; r++)
+        {
+            sum+= values[r];
+        }
+        ave = sum / values.length;
+        return ave;
+    }
+
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class SafeInput {
+import java.util.Scanner;
+
+public class SafeInput {
+    /**
+     *
+     * @param pipe The Scanner instance to use for the console input
+     * @param prompt A string that tells the user what to input
+     * @param low The lower bound for the range of the valid values
+     * @param high The upper bound for the ranger of valid values
+     * @return an integer within the range
+     */
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+        int retVal = low - 1;
+        String trash = "";
+        boolean done = false;
+
+        do {
+
+            System.out.println(prompt + "[" + low + " - " + high +"]: ");
+            if (pipe.hasNextInt()) {
+                retVal = pipe.nextInt();
+                pipe.nextLine(); // clear the newline form the buffer
+                if (retVal >= low && retVal <= high) {
+                    System.out.println("\nYou said your val is " + retVal);
+                    done = true;
+                } else {
+                    System.out.println("You said your ret val is " + retVal + " but that is out of range [" + low + " - " + high +"]: ");
+                }
+            } else // don't have an int
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an integer not " + trash);
+            }
+        } while (!done);
+
+        return retVal;
+    }
 }
